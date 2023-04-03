36b33f073aa6e4763d9d3b2bc6ac8272af78c288
ymf <1950714773@qq.com>
2023-04-03 20:30:16 +0800

    fixed rollup打包未转成es5语法导致fei-editor组件引入组件库报错

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -7,7 +7,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import replace from '@rollup/plugin-replace'
 import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 
 const name = 'FUI'
 
